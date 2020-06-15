f19524d2506c75d52a0824f504bc812a11b6f75c
Siddhant Rattu <sidrattu@gmail.com>
2020-06-16 01:18:34 +0530

    Update Five rivers.R

--- Code/Five rivers.R
+++ Code/Five rivers.R
@@ -1,2 +1,10 @@
 #Five rivers
 
+
+#	Douro River - 
+#Fifteen dams have been built on the Douro to regulate the water flow, generate hydroelectric power, and allow navigation through locks
+# Vltava - 
+# Nine hydroelectric dams have been built on the Vltava south of Prague to regulate the water flow and generate hydroelectric power, starting in the 1950s
+# Rhine River
+# Volga River
+# Danube River
